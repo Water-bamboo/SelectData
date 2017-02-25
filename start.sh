45e7c5c27644470ed657fba0c675ecd3987392b1
Tangcheng <helloworld.tang@qq.com>
2017-02-25 23:39:49 +0800

    refactor

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup java -jar -Dspring.profiles.active=prod,swagger -Dserver.port=80  -Dmanagement.security.enabled=true selectdata-0.0.1-SNAPSHOT.jar 2>error.log 1>/dev/null &
+nohup java -jar -Dspring.profiles.active=prod,swagger -Dserver.port=80 -Dmanagement.security.enabled=true selectdata-0.0.1-SNAPSHOT.jar 2>error.log 1>/dev/null &
 
 
 
